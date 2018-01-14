8b641916803a06dbfd111988962d02cd7924bb5c
sazzad23 <eresh24@gmail.com>
2018-01-15 05:14:58 +0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    //Background height fix
+//    function backgroundFix(){
+//        var getBodyHeight = $('body').height();
+//        
+//        $('#main').height(getBodyHeight);
+//        
+//    }
+//    backgroundFix(); //call function
+//        $(window).resize(function(){
+//            
+//        setTimeout(function(){
+//            backgroundFix(); //call function on window rezize
+//        },1000);
+//         
+//    });
+
+    
+});
